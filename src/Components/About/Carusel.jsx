ad01459f02ba79e2d14e75b3e8f9da0f6e0d93c8
xasanAbdurazakov <xasano5o@xasanAbdurazakovs-MacBook-Pro.local>
2024-01-30 08:53:30 -0800

    update

--- src/Components/About/Carusel.jsx
+++ src/Components/About/Carusel.jsx
@@ -8,17 +8,10 @@ const CaruselNav = () => {
         <div className="self-stretch text-black text-[15px] font-normal font-['Syne'] tracking-wide">Introducing stress-free roundtrip reservations <br />for airoports, events and everyday journeys</div>
       </div>
       <div>
-        
+
       </div>
     </div>
-    // <div className="w-full h-[390px] grid grid-cols-2 bg-stone-100 rounded-bl-[25px] rounded-br-[25px]">
-    //   <div className="px-[23px] py-[15px] left-[23px] top-[230px]  flex-col justify-start items-start gap-[13px] inline-flex">
-    //     <div className="self-stretch text-black text-2xl font-semibold font-['Syne'] uppercase tracking-widest">Complete your journey <br />whit scheduled ROUndtrips</div>
-    //     <div className="self-stretch text-black text-[15px] font-normal font-['Syne'] tracking-wide">Introducing stress-free roundtrip reservations <br />for airoports, events and everyday journeys</div>
-    //   </div>
-    //   <div className="w-[479px] h-[369px] left-[545px] top-0 " />
-    //   anime
-    // </div>
+
   )
 }
 
